6cc333cf29b631ccdb9fb1ed1f5e07c464da3810
ppp5649 <ppp5649@naver.com>
2022-05-07 20:36:08 +0900

    22/05/07 웹 서버 구동을 위한 staticfiles 정리

--- roadmap/settings.py
+++ roadmap/settings.py
@@ -170,6 +170,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static', ]
 # STATICFILES의 디렉토리를 지정해주는 작업 매우중요 !
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
