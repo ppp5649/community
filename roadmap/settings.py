9d68108874908dff64eb46edb64500ae16bce03e
ppp5649 <ppp5649@naver.com>
2022-05-08 15:59:31 +0900

    22/05/08 staticfiles 경로 수정

--- roadmap/settings.py
+++ roadmap/settings.py
@@ -166,8 +166,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'staticfiles/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'staticfiles', ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # STATICFILES의 디렉토리를 지정해주는 작업 매우중요 !
 
